9d107e6d9f25181f2782b387b1d89be7e641ef36
sushil2817 <tony281723@gmail.com>
2024-04-24 08:33:25 +0530

    Revision Javascript basic

---
+++ 01Project/script.js
@@ -0,0 +1,16 @@
+const accountID  = 144553
+let accountEmail = "sushil@example.com"
+var accountPassword = "sushil234"
+accountCity = "Jaipur"
+
+/*
+    var use nhi krna chahiye 
+*/
+
+// accountID = 2
+accountEmail = "sk@sc.com"
+accountPassword = "212121"
+accountCity = "Nawa"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
---
+++ 02Project/script.js
@@ -0,0 +1,24 @@
+"user strict"; // treat all js code as newer version
+
+// alert(3+3)// we are using node js not browser
+
+// console.log(3+3); // code readability should be high
+
+let name = "sushil"
+let age = 19
+let isloggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ''/""
+// boolean
+// null => standalone value
+// undefined 
+// symbol => unique ness
+
+// object
+
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
