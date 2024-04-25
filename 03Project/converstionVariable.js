fca14eadc17bfc2576ef603c08a0785b5ee5c8ca
sushil2817 <tony281723@gmail.com>
2024-04-25 08:01:43 +0530

    Conversion variable in js

---
+++ 03Project/converstionVariable.js
@@ -0,0 +1,33 @@
+let score = true
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// 35 -> Number
+// 35abc -> type number, value NaN
+// true ->1, false->0
+// object -> value NaN
+// null -> value 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+
+// console.log(booleanIsLoggedIn);
+
+// 1->true, 0->false
+// ""->false
+// "s"->true
+
+let someNumber  = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
