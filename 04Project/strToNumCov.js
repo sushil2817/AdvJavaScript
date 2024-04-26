d12c939fbd07d986dacf52e380aaa108a7effc13
sushil2817 <tony281723@gmail.com>
2024-04-26 08:16:06 +0530

    learning String to Number Conversion operations

---
+++ 04Project/strToNumCov.js
@@ -0,0 +1,41 @@
+let str1 = "hello"
+let str2 = " Sushil"
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);//string
+// console.log(1+2+"2");//number+string
+// console.log(1+"2"+2);//string
+
+// console.log(true);//true
+// console.log(+true);1
+// console.log(true+);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100;
+// ++gameCounter;
+console.log(++gameCounter);
+console.log(gameCounter++);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
