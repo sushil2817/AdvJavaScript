df705b2837d929b06f055be8352607c731e095fd
sushil2817 <tony281723@gmail.com>
2024-05-10 11:17:18 +0530

    understanding javascript flow

---
+++ 13Project/script.js
@@ -0,0 +1,47 @@
+// if
+
+const isUserloggedIn = true
+const temprature = 41
+
+// if(temprature === 41){
+//      console.log("less then 50");
+// }else{
+//         console.log("grater then 50");
+// }
+    
+// <,>,<=,>=,==,!=,===,!==
+
+// const score = 200;
+
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`USer power: ${power}`);
+// }
+// console.log(`USer power: ${power}`);
+
+
+const balance = 1000
+
+// if(balance>500) console.log("test"), console.log(test2); don't do this kind of thing
+
+
+if(balance < 500){
+          console.log("less than");
+}else if(balance < 750){
+      console.log("less then 750");
+}else if(balance < 900){
+    console.log("less then 900");
+}else if(balance < 1200){
+    console.log("less then 1200");
+}
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedIFromGoogle = false
+
+if(userLoggedIn && debitCard && loggedIFromGoogle ){
+    console.log("Allow to buy course");
+}
+
+
