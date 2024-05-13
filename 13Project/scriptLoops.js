c36434ebbb2122254b3d45c776ca979ec2b77926
sushil2817 <tony281723@gmail.com>
2024-05-13 07:57:59 +0530

    Loops in javascript

---
+++ 13Project/scriptLoops.js
@@ -0,0 +1,37 @@
+// for loop
+
+// for (let i = 1; i <=10; i++) {
+//     if(i==5){
+//         console.log("5 is best");
+//     }
+//       console.log(i);
+// }
+
+// for (let i = 0; i<=10; i++) {
+
+//       for(let j=0; j<=10; j++){
+//           console.log("i -> "+i+" j -> "+j);
+//       }
+// }
+
+// break and continue
+
+// for (let i = 1; i <=20 ; i++) {
+
+//     if(i==5){
+//         console.log("value is 5");
+//         break
+//     }
+//     console.log(`Value of i is ${i}`);
+    
+// }
+
+for (let i = 1; i <=20 ; i++) {
+
+    if(i==5){
+        console.log("value is 5");
+        continue
+    }
+    console.log(`Value of i is ${i}`);
+    
+}
