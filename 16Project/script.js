eda2a79246888083cb8c1a2e13b77eed3017e2e0
sushil2817 <tony281723@gmail.com>
2024-05-23 08:32:40 +0530

    created a color Switcher

---
+++ 16Project/script.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+    console.log(button);
+    button.addEventListener("click", (e) => {
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.value);
+        if (e.target.id === "gray") {
+            body.style.backgroundColor = e.target.id;
+        } else if (e.target.id === "white") {
+            body.style.backgroundColor = e.target.id;
+        } else if (e.target.id === "blue") {
+            body.style.backgroundColor = e.target.id;
+        } else if (e.target.id === "yellow") {
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+});
