8dab8aa63595f74227ba91ec9452af47fec0c669
sushil2817 <tony281723@gmail.com>
2024-05-29 08:14:05 +0530

    making a project called guess my number

--- 17Project/script.js
+++ 17Project/script.js
@@ -1,37 +1,26 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form");
 // this usecase will give you empty value
 // const weight = parseInt(document.querySelector('#weight').value)
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-form.addEventListener('submit', (e)=>{
-      e.preventDefault();
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+    const result = document.querySelector("#results");
+    if (height === "" || height < 0 || isNaN(height)) {
+        result.innerHTML = ` <br> Please give a valid height ${height}`;
+    } else if (weight === "" || weight < 0 || isNaN(weight)) {
+        result.innerHTML = ` <br>Please give a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
-      const height = parseInt(document.querySelector('#height').value)
-      const weight = parseInt(document.querySelector('#weight').value)
-      const result = document.querySelector('#results')
-         if(height === '' || height < 0 || isNaN(height)){
-            result.innerHTML = ` <br> Please give a valid height ${height}`
-         } else if(weight === '' || weight < 0 || isNaN(weight)){
-            result.innerHTML = ` <br>Please give a valid weight ${weight}`
-         } else {
-           const bmi =  (weight/((height*height)/10000)).toFixed(2)
-            
-                
-             
-            if(bmi>=0 && bmi <= 18.6){
-                
-                  result.innerHTML =   ` <br><span> Your bmi is ${bmi}</span>  <br> You are Under Wight trying to eat Food`
-            }  else if(bmi >= 18.6 && bmi <= 24.9){
-                
-                result.innerHTML = ` <br><span> Your bmi is ${bmi}</span> <br>You are safe `
-            } else {
-                
-                result.innerHTML = ` <br><span> your bmi is ${bmi}</span> <br> You in Overweight trying to avoid food `
-            }
-
-         }
-
-         
-
-
-})
+        if (bmi >= 0 && bmi <= 18.6) {
+            result.innerHTML = ` <br><span> Your bmi is ${bmi}</span>  <br> You are Under Wight trying to eat Food`;
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
+            result.innerHTML = ` <br><span> Your bmi is ${bmi}</span> <br>You are safe `;
+        } else {
+            result.innerHTML = ` <br><span> your bmi is ${bmi}</span> <br> You in Overweight trying to avoid food `;
+        }
+    }
+});
---
+++ 19Project/script.js
@@ -0,0 +1,44 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const logOrHi = document.querySelector('.logOrHi')
+const startOver = document.querySelector('.resultParas')
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+if(playGame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault()
+    })
+}
+
+
+function validateGuess(guess){
+// 
+}
+
+function checkGuess(guess){
+// 
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+    // 
+}
+
+function endGame(){
+
+}
+
+function newGame(){
+
+}
