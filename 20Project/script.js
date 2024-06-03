7746acaa881b6ddd62eb7e3908aad1a080b376e8
sushil2817 <tony281723@gmail.com>
2024-06-03 07:48:21 +0530

    async and sync javascript

--- 20Project/script.js
+++ 20Project/script.js
@@ -16,11 +16,30 @@
 // altKey, ctrlKey, shiftKey, keyCode 
 
 
-document.getElementById('images').addEventListener('click',(e)=>{
-     console.log('code clicked');
-},false)
+// document.getElementById('images').addEventListener('click',(e)=>{
+//      console.log('code clicked');
+// // },false)
 
-document.getElementById('owl').addEventListener('click',(e)=>{
-     console.log('owl clicked');
-     e.stopPropagation()
+// document.getElementById('owl').addEventListener('click',(e)=>{
+//      console.log('owl clicked');
+//      e.stopPropagation()
+// },false)
+
+// document.getElementById('google').addEventListener('click',()=>{
+//      e.preventDefault()
+//      e.stopPropagation()
+//      console.log("google clicked");
+// },false)
+
+
+
+document.querySelector('#images').addEventListener('click',(e)=>{
+       console.log(e.target.parentNode);
+
+       if(e.target.target == 'IMG'){
+          let removeIT = e.target.parentNode
+          removeIT.remove()
+       }
+       
+     // removeIT.parentNode.removeChild(removeIT)
 },false)
---
+++ 21Project/scritp.js
@@ -0,0 +1,2 @@
+// javascript is synchronous , singleThreaded language
+// execution context => each opertatiojnwait for the last one to complete execution
