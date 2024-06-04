88c886654027e08739f928e1268057a5c256e1bc
sushil2817 <tony281723@gmail.com>
2024-06-04 08:12:22 +0530

    leraning time function

--- 21Project/scritp.js
+++ 21Project/scritp.js
@@ -1,2 +1,9 @@
 // javascript is synchronous , singleThreaded language
 // execution context => each opertatiojnwait for the last one to complete execution
+
+
+const saySushil = function(){
+    console.log("sushil ");
+}
+
+setTimeout(saySushil,2000)
