bda1cd083b666a6531fecb546170907d4fae9a97
sushil2817 <tony281723@gmail.com>
2024-07-01 22:18:16 +0530

    javascript move to completoing

--- 27Project/mathPY.js
+++ 27Project/mathPY.js
@@ -23,6 +23,7 @@ console.log(Object.getOwnPropertyDescriptor(chai,"name"));
 for (let [key,value] of Object.entries(chai)){
     if(typeof value !== "function"){
         console.log(`${key} : ${value}`);
-    }
-
+     }
 }
+
+// this week javascript will be complete
