594a4819fc4489423f12ba72ee53e9cb1b2de90a
sushil2817 <tony281723@gmail.com>
2024-07-05 08:36:54 +0530

    complete javascript

--- 29Project/lexicalscopingandclosuer.js
+++ 29Project/lexicalscopingandclosuer.js
@@ -7,12 +7,34 @@
 // }
 // init();
 
-
+// --------------- lexical scoping -------------
 function outer(){
     let username = "sushil"
+    console.log(secret);
     function inner(){
-        console.log(username);
+        let secret = "my123"
+        console.log("inner",username);
     }
+    function innerTwo(){
+        console.log("innerTwo",username);
+        // console.log(secret);
+    }
+    inner();
+    innerTwo()
 }
 outer( )
-console.log("TOO Outer", username);
+// console.log("TOO Outer", username);
+
+// -------------------- closur---------------
+
+// function makeFunc() {
+//     const name = "Mozilla";
+//     function displayName() {
+//     console.log(name);
+//     }
+//     return displayName;
+// }
+
+// const myFunc = makeFunc();
+// myFunc();
+
